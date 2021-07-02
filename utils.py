6d0be15af3a001835009b8178d7bf80b6577b8b2
arvens1990 <arvens1990@gmail.com>
2021-07-02 10:55:30 -0400

    utils ready.

--- utils.py
+++ utils.py
@@ -6,6 +6,8 @@ import numpy as np
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import TweetTokenizer
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
 
 
 def process_tweet(tweet):
@@ -70,3 +72,310 @@ def build_freqs(tweets, ys):
                 freqs[pair] = 1
 
     return freqs
+
+def test_lookup(func):
+    freqs = {('sad', 0): 4,
+             ('happy', 1): 12,
+             ('oppressed', 0): 7}
+    word = 'happy'
+    label = 1
+    if func(freqs, word, label) == 12:
+        return 'SUCCESS!!'
+    return 'Failed Sanity Check!'
+
+
+def lookup(freqs, word, label):
+    '''
+    Input:
+        freqs: a dictionary with the frequency of each pair (or tuple)
+        word: the word to look up
+        label: the label corresponding to the word
+    Output:
+        n: the number of times the word with its corresponding label appears.
+    '''
+    n = 0  # freqs.get((word, label), 0)
+
+    pair = (word, label)
+    if (pair in freqs):
+        n = freqs[pair]
+
+    return n
+
+# From: https://matplotlib.org/3.1.1/gallery/statistics/confidence_ellipse.html#sphx-glr-gallery-statistics-confidence-ellipse-py
+
+
+def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
+    """
+    Create a plot of the covariance confidence ellipse of `x` and `y`
+    Parameters
+    ----------
+    x, y : array_like, shape (n, )
+        Input data.
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+    Other parameters
+    ----------------
+    kwargs : `~matplotlib.patches.Patch` properties
+    """
+    if x.size != y.size:
+        raise ValueError("x and y must be the same size")
+
+    cov = np.cov(x, y)
+    pearson = cov[0, 1] / np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensionl dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0),
+                      width=ell_radius_x * 2,
+                      height=ell_radius_y * 2,
+                      facecolor=facecolor,
+                      **kwargs)
+
+    # Calculating the stdandard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = np.mean(x)
+
+    # calculating the stdandard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = np.mean(y)
+
+    transf = transforms.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
+
+def count_tweets(result, tweets, ys):
+    '''
+    Input:
+        result: a dictionary that will be used to map each pair to its frequency
+        tweets: a list of tweets
+        ys: a list corresponding to the sentiment of each tweet (either 0 or 1)
+    Output:
+        result: a dictionary mapping each pair to its frequency
+    '''
+
+    ###
+    for y, tweet in zip(ys, tweets):
+        for word in process_tweet(tweet):
+            # define the key, which is the word and label tuple
+            pair = (word, y)
+
+            # if the key exists in the dictionary, increment the count
+            if pair in result:
+                result[pair] += 1
+
+            # else, if the key is new, add it to the dictionary and set the count to 1
+            else:
+                result[pair] = 1
+    ###
+
+    return result
+
+
+def train_naive_bayes(freqs, train_x, train_y):
+    '''
+    Input:
+        freqs: dictionary from (word, label) to how often the word appears
+        train_x: a list of tweets
+        train_y: a list of labels correponding to the tweets (0,1)
+    Output:
+        logprior: the log prior. (equation 3 above)
+        loglikelihood: the log likelihood of you Naive bayes equation. (equation 6 above)
+    '''
+    loglikelihood = {}
+    logprior = 0
+
+    ###
+
+    # calculate V, the number of unique words in the vocabulary
+    vocab = set([pair[0] for pair in freqs.keys()])
+    V = len(vocab)
+
+    # calculate N_pos and N_neg
+    N_pos = N_neg = 0
+    for pair in freqs.keys():
+        # if the label is positive (greater than zero)
+        if pair[1] > 0:
+
+            # Increment the number of positive words by the count for this (word, label) pair
+            N_pos += freqs.get(pair, 0)
+
+        # else, the label is negative
+        else:
+
+            # increment the number of negative words by the count for this (word,label) pair
+            N_neg += freqs.get(pair, 0)
+
+    # Calculate D, the number of documents
+    D = len(train_y)
+
+    # Calculate D_pos, the number of positive documents (*hint: use sum(<np_array>))
+    D_pos = np.sum(train_y==1.)
+
+    # Calculate D_neg, the number of negative documents (*hint: compute using D and D_pos)
+    D_neg = np.sum(train_y==0.)
+
+    # Calculate logprior
+    logprior = np.log((D_pos)/(D_neg))
+
+    # For each word in the vocabulary...
+    freq_pos = 0
+    freq_neg = 0
+    for word in vocab:
+        # get the positive and negative frequency of the word
+        if (word, 1.) in freqs:
+            freq_pos = freqs[(word, 1.)]
+        if (word, 0.) in freqs:
+            freq_neg = freqs[(word, 0.)]
+
+        # calculate the probability that each word is positive, and negative
+        p_w_pos = (freq_pos + 1)/(N_pos + V)
+        p_w_neg = (freq_neg + 1)/(N_neg + V)
+
+        # calculate the log likelihood of the word
+        loglikelihood[word] = np.log(p_w_pos/p_w_neg)
+
+    ###
+
+    return logprior, loglikelihood
+
+def naive_bayes_predict(tweet, logprior, loglikelihood):
+    '''
+    Input:
+        tweet: a string
+        logprior: a number
+        loglikelihood: a dictionary of words mapping to numbers
+    Output:
+        p: the sum of all the logliklihoods of each word in the tweet (if found in the dictionary) + logprior (a number)
+
+    '''
+    ### 
+    # process the tweet to get a list of words
+    word_l = process_tweet(tweet)
+
+    # initialize probability to zero
+    p = 0
+
+    # add the logprior
+    p += logprior
+
+    for word in word_l:
+
+        # check if the word exists in the loglikelihood dictionary
+        if word in loglikelihood:
+            # add the log likelihood of that word to the probability
+            p += loglikelihood[word]
+
+    ###
+
+    return p
+
+def test_naive_bayes(test_x, test_y, logprior, loglikelihood):
+    """
+    Input:
+        test_x: A list of tweets
+        test_y: the corresponding labels for the list of tweets
+        logprior: the logprior
+        loglikelihood: a dictionary with the loglikelihoods for each word
+    Output:
+        accuracy: (# of tweets classified correctly)/(total # of tweets)
+    """
+    accuracy = 0  # return this properly
+
+    ### 
+    y_hats = []
+    for tweet in test_x:
+        # if the prediction is > 0
+        if naive_bayes_predict(tweet, logprior, loglikelihood) > 0:
+            # the predicted class is 1
+            y_hat_i = 1
+        else:
+            # otherwise the predicted class is 0
+            y_hat_i = 0
+
+        # append the predicted class to the list y_hats
+        y_hats.append(y_hat_i)
+
+    # error is the average of the absolute values of the differences between y_hats and test_y
+    error = (1/len(y_hats))*np.absolute(np.array(test_y) - np.array(y_hats)).sum()
+
+    # Accuracy is 1 minus the error
+    accuracy = 1 - error
+
+    ### 
+
+    return accuracy
+
+# UNQ_C8 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+def get_ratio(freqs, word):
+    '''
+    Input:
+        freqs: dictionary containing the words
+        word: string to lookup
+
+    Output: a dictionary with keys 'positive', 'negative', and 'ratio'.
+        Example: {'positive': 10, 'negative': 20, 'ratio': 0.5}
+    '''
+    pos_neg_ratio = {'positive': 0, 'negative': 0, 'ratio': 0.0}
+    ### 
+    # use lookup() to find positive counts for the word (denoted by the integer 1)
+    pos_neg_ratio['positive'] = lookup(freqs, word, 1)
+
+    # use lookup() to find negative counts for the word (denoted by integer 0)
+    pos_neg_ratio['negative'] = lookup(freqs, word, 0)
+
+    # calculate the ratio of positive to negative counts for the word
+    pos_neg_ratio['ratio'] = (pos_neg_ratio['positive']+1)/(pos_neg_ratio['negative']+1)
+    ### 
+    return pos_neg_ratio
+
+# UNQ_C9 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+def get_words_by_threshold(freqs, label, threshold):
+    '''
+    Input:
+        freqs: dictionary of words
+        label: 1 for positive, 0 for negative
+        threshold: ratio that will be used as the cutoff for including a word in the returned dictionary
+    Output:
+        word_list: dictionary containing the word and information on its positive count, negative count, and ratio of positive to negative counts.
+        example of a key value pair:
+        {'happi':
+            {'positive': 10, 'negative': 20, 'ratio': 0.5}
+        }
+    '''
+    word_list = {}
+
+    ### 
+    for key in freqs.keys():
+        word, _ = key
+
+        # get the positive/negative ratio for a word
+        pos_neg_ratio = get_ratio(freqs, word)
+
+        # if the label is 1 and the ratio is greater than or equal to the threshold...
+        if label == 1 and pos_neg_ratio["ratio"] >= threshold:
+
+            # Add the pos_neg_ratio to the dictionary
+            word_list[word] = pos_neg_ratio
+
+        # If the label is 0 and the pos_neg_ratio is less than or equal to the threshold...
+        elif label == 0 and pos_neg_ratio["ratio"] <= threshold:
+
+            # Add the pos_neg_ratio to the dictionary
+            word_list[word] = pos_neg_ratio
+
+        # otherwise, do not include this word in the list (do nothing)
+
+    ### 
+    return word_list
